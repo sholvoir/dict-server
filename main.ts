7b8a10ea2e9bca74ef150aa52df49247953d7dad
sholvoir <sovar.he@gmail.com>
2023-10-04 10:19:40 -0400

    dotevn

--- main.ts
+++ main.ts
@@ -5,8 +5,6 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
-import "$std/dotenv/load.ts";
-
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
