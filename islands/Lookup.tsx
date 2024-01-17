3e9f6a9620927fb31842b298adf1af684f555e52
sholvoir <sovar.he@gmail.com>
2024-01-16 20:22:24 -0500

    tips

--- islands/Lookup.tsx
+++ islands/Lookup.tsx
@@ -9,9 +9,14 @@ const inputNames = ['word','pic','trans','sound','phonetic'];
 type InputName = typeof inputNames[number];
 
 export default function Lookup() {
-    const prompt = useSignal('');
     const inputs: Record<InputName, Signal<string>> = {};
     for (const name of inputNames) inputs[name] = useSignal('');
+    const tips = useSignal('');
+    const showTips = (content: string) => {
+        tips.value = content;
+        setTimeout(hideTips, 3000);
+    };
+    const hideTips = () => tips.value = '';
     const handleInput = (ev: Event) => {
         const target = ev.target as HTMLInputElement;
         inputs[target.name].value = target.value;
@@ -23,37 +28,28 @@ export default function Lookup() {
             inputs['phonetic'].value = dic.phonetic!;
             inputs['trans'].value = dic.trans!;
             inputs['sound'].value = dic.sound!;
-            prompt.value = '';
-        } else {
-            prompt.value = await res.text();
-        }
+            inputs['pic'].value = dic.pic!
+        } else showTips(await res.text());
     }
     const handlePlayClick = () => {
-        if (inputs['sound'].value) {
-            try {
-                (new Audio(inputs['sound'].value)).play();
-                prompt.value = '';
-            } catch (e) {
-                prompt.value = e.toString();
-            }
-        } else {
-            prompt.value = 'no sound to play!';
-        }
+        if (!inputs['sound'].value) return showTips('no sound to play!');
+        try { (new Audio(inputs['sound'].value)).play(); }
+        catch (e) { showTips(e.toString()); }
     }
     const handleUpdateClick = async () => {
-        const res = await fetch(`${baseApi}/${encodeURIComponent(inputs['word'].value)}`, {
-            method: 'PATCH',
-            cache: 'no-cache',
-            body: JSON.stringify({ trans: inputs['trans'].value, sound: inputs['sound'].value, phonetic: inputs['phonetic'].value })
-        });
-        if (res.ok) {
-            prompt.value = (`success upate word "${inputs['word'].value}"!`);
-        } else {
-            prompt.value = (await res.text());
-        }
+        const dict: IDict = {};
+        if (inputs['pic'].value) dict.trans = inputs['pic'].value;
+        if (inputs['trans'].value) dict.trans = inputs['trans'].value;
+        if (inputs['sound'].value) dict.trans = inputs['sound'].value;
+        if (inputs['phonetic'].value) dict.trans = inputs['phonetic'].value;
+        const res = await fetch(`${baseApi}/${encodeURIComponent(inputs['word'].value)}`,
+            { method: 'PATCH', cache: 'no-cache', body: JSON.stringify(dict) }
+        );
+        if (res.ok) showTips(`success upate word "${inputs['word'].value}"!`);
+        else showTips('Network Error!');
     }
     return <div class="[&>div]:m-1 [&>div]:flex [&>div]:gap-2">
-        <div class="text-red-500">{prompt}</div>
+        <div class="absolute top-0 inset-x-[10%] bg-[rgba(255,255,0,0.5)] text-center rounded-md" onClick={hideTips}>{tips.value}</div>
         <div>
             <input type="text" name="word" placeholder="word"
                 class="grow border px-2" value={inputs['word'].value}
@@ -63,23 +59,22 @@ export default function Lookup() {
         </div>
         <div>
             <input type="text" name="phonetic" placeholder="phonetic"
-                class="border px-2 mr-2" value={inputs['phonetic'].value} onInput={handleInput}/>
-            <input type="text" name="sound" placeholder="sound"
-                class="grow border px-2" value={inputs['sound'].value} onInput={handleInput} />
-            <IconPlayerPlayFilled class="w-6 h-6" onClick={handlePlayClick} />
+                class="border px-2 mr-1" value={inputs['phonetic'].value} onInput={handleInput}/>
+            <input type="text" name="trans" placeholder="trans"
+                class="grow border px-2" value={inputs['trans'].value}
+                onInput={handleInput} />
         </div>
         <div class="justify-center">
             <img class="max-h-[480px] max-w-[720px]" src={inputs['pic'].value || noImage}/>
         </div>
         <div>
-            <input type="text" name="pic" placeholder="pic"
-                class="grow border px-2" value={inputs['pic'].value}
-                onInput={handleInput} />
+            <textarea name="pic" placeholder="pic" class="grow border px-2"
+                value={inputs['pic'].value} onInput={handleInput}/>
         </div>
         <div>
-            <input type="text" name="trans" placeholder="trans"
-                class="grow border px-2" value={inputs['trans'].value}
-                onInput={handleInput} />
+            <textarea name="sound" placeholder="sound" class="grow border h-48 px-2"
+                value={inputs['sound'].value} onInput={handleInput}/>
+            <button onClick={handlePlayClick}><IconPlayerPlayFilled class="w-6 h-6"/></button>
         </div>
         <div class="justify-end">
             <button type="botton" class="w-20 border rounded-md px-2 bg-blue-800 text-white disabled:opacity-50 disabled:bg-gray-300"
