5c0cf53c7e3248fa0fd1c80cba60c7ac6c8c0110
sholvoir <sovar.he@gmail.com>
2024-01-22 18:35:33 -0500

    youdao trans update

--- lib/youdao.ts
+++ lib/youdao.ts
@@ -1,11 +1,12 @@
+// deno-lint-ignore-file no-explicit-any
 const baseUrl = 'https://dict.youdao.com/jsonapi';
 
 export async function trans(en: string): Promise<string|undefined> {
     const resp = await fetch(`${baseUrl}?q=${en}`);
     if (!resp.ok) return undefined;
-    const tr = (await resp.json()).individual?.trs?.[0];
-    if (!tr) return undefined;
-    return `${tr.pos}${tr.tran?.replaceAll('，', ',').replaceAll('；',';')}`;
+    const trs = (await resp.json()).individual?.trs;
+    if (!trs || !trs.length) return undefined;
+    return trs.map((tr: any) => `${tr.pos}${tr.tran?.replaceAll('，', ',').replaceAll('；',';')}`).join(' ');
 }
 
-if (import.meta.main) console.log(await trans(Deno.args[0]));
+if (import.meta.main) for (const en of Deno.args) console.log(await trans(en));
