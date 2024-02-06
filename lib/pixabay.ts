ce929f4a42f2f9857fb45ba647fd5745724e12dd
sholvoir <sovar.he@gmail.com>
2024-02-06 13:06:06 -0500

    horizontal

--- lib/pixabay.ts
+++ lib/pixabay.ts
@@ -3,7 +3,7 @@ import { type IDict } from './idict.ts';
 const baseUrl = 'https://pixabay.com/api/';
 const key = Deno.env.get('PIXABAY_KEY');
 export const getPic = async (word: string): Promise<IDict> => {
-    const resp = await fetch(`${baseUrl}?key=${key}&q=${encodeURIComponent(word)}`);
+    const resp = await fetch(`${baseUrl}?key=${key}&q=${encodeURIComponent(word)}&orientation=horizontal`);
     if (resp.ok) {
         const content = await resp.json();
         if (content.hits?.length) return { pic:  content.hits[0].previewURL.replace('_150.', '_1280.') };
