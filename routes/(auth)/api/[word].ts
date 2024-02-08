8a8984ca39f91c7b488957a96b188c7043e54000
sholvoir <sovar.he@gmail.com>
2024-02-08 08:35:32 -0500

    dataurl

--- routes/(auth)/api/[word].ts
+++ routes/(auth)/api/[word].ts
@@ -4,6 +4,7 @@ import { getAll as youdaoAll } from '../../../lib/youdao.ts';
 import { getPhoneticSound as dictPhoneticSound } from "../../../lib/dictionary.ts";
 import { getSound as websterSound } from "../../../lib/webster.ts";
 import { getPic as pixabayGetPic } from '../../../lib/pixabay.ts';
+import { blobToBase64 } from "../../../lib/blob.ts";
 
 const resInit = { headers: { "Content-Type": "application/json" } };
 const category = 'dict';
@@ -13,7 +14,7 @@ const ok = new Response(undefined, { status: STATUS_CODE.OK });
 const internalServerError = new Response(undefined, { status: STATUS_CODE.InternalServerError });
 
 export const handler: Handlers = {
-    async GET(_req, ctx) {
+    async GET(req, ctx) {
         try {
             const word = decodeURIComponent(ctx.params.word.trim());
             if (!word) return badRequest;
@@ -21,7 +22,6 @@ export const handler: Handlers = {
             const res = await kv.get([category, word]);
             const value = res.value as IDict;
             if (!value) return notFound;
-            if (value.sound?.startsWith('data:')) value.sound = '';
             if (value.pic && !value.pic.startsWith('https://cdn')) value.pic = '';
             let modified = false;
             if (!value.sound && (value.sound = (await websterSound(word)).sound)) modified = true;
@@ -37,6 +37,14 @@ export const handler: Handlers = {
                 if (!value.phonetic && (value.phonetic = dict.phonetic)) modified = true;
             }
             if (!value.pic && (value.pic = (await pixabayGetPic(word)).pic)) modified = true;
+            if (value.sound?.startsWith('http')) {
+                const reqInit = { headers: { 'User-Agent': req.headers.get('User-Agent') || 'Thunder Client (https://www.thunderclient.com)'} }
+                const resp = await fetch(value.sound, reqInit);
+                if (resp.ok) {
+                    value.sound = await blobToBase64(await resp.blob());
+                    modified = true;
+                } else console.log(resp.status, await resp.text());
+            }
             if (modified) await kv.set([category, word], value);
             kv.close();
             return new Response(JSON.stringify(value), resInit);
