715870a41490cb14a16e133c359b93cc5afea6b9
sholvoir <sovar.he@gmail.com>
2024-02-13 19:18:43 -0500

    only me

--- routes/(auth)/api/_middleware.ts
+++ routes/(auth)/api/_middleware.ts
@@ -1,6 +1,6 @@
 import { FreshContext } from "$fresh/server.ts";
 
 export async function handler(req: Request, ctx: FreshContext) {
-    if (req.method == 'GET' || ctx.state.user) return await ctx.next();
+    if (req.method == 'GET' || ctx.state.user == 'c292YXIuaGVAZ21haWwuY29t') return await ctx.next();
     return new Response(undefined, { status: 401 });
 }
