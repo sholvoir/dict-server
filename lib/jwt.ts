15ce3c92b084a3a078d661020d78238e901e4429
sholvoir <sovar.he@gmail.com>
2024-07-12 10:55:41 -0400

    app-key

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -1,4 +1,4 @@
 import { JWT } from "@sholvoir/generic/jwt";
 
 export const jwt = new JWT({ iss: 'micit.co', sub: 'dict' });
-await jwt.importKey(Deno.env.get('DICT_KEY')!);
+await jwt.importKey(Deno.env.get('APP_KEY')!);
