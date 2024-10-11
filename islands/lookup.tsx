bde2681e24145987535da195d314f8930a37a946
sholvoir <sovar.he@gmail.com>
2024-10-11 18:39:51 -0400

    vocabulary/0.0.22

--- islands/lookup.tsx
+++ islands/lookup.tsx
@@ -10,7 +10,7 @@ import ButtonBase from "@sholvoir/components/islands/button-base.tsx";
 import Button from "@sholvoir/components/islands/button-ripple.tsx";
 
 const baseApi = '/api';
-const vocabularyUrl = 'https://www.micit.co/vocabulary/0.0.21/vocabulary.txt';
+const vocabularyUrl = 'https://www.micit.co/vocabulary/0.0.22/vocabulary.txt';
 const vocabulary: Array<string> = [];
 
 export default function Lookup() {
