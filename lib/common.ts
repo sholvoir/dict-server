d2209558c7760e20b7fa5cfea2a45b5eeb89a60b
sholvoir <sovar.he@gmail.com>
2025-01-06 12:10:38 -0500

    0.4.4

--- lib/common.ts
+++ lib/common.ts
@@ -1,5 +1,5 @@
 // deno-lint-ignore-file no-explicit-any
 import { IDict } from "./idict.ts";
 
-export const VOCABULARY_URL = 'https://www.micit.co/vocabulary/vocabulary-0.1.2.txt';
+export const VOCABULARY_URL = 'https://www.micit.co/vocabulary/vocabulary-0.1.3.txt';
 export interface IDictP extends IDict { modified?: any }
