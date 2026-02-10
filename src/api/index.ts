bb9a426421bdcddf0d1fd9df698c6087b058e444
sholvoir <sovar.he@gmail.com>
2026-02-09 21:24:30 -0500

    0.7.1

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,22 +1,12 @@
-import { DOMParser } from "@b-fuze/deno-dom";
 import type { Hono } from "hono";
 import admin from "../mid/admin.ts";
 import auth from "../mid/auth.ts";
 
-const url = "https://www.micinfotech.com/dict/";
+const url = "https://www.micinfotech.com/dict";
 
 const apply = (app: Hono) => {
-   app.get("/", auth, admin, async (c) => {
-      const res = await fetch(`${url}index.html`);
-      if (!res.ok) return res;
-      const text = await res.text();
-      const doc = new DOMParser().parseFromString(text, "text/html");
-      if (!doc) return c.text("Error parsing HTML");
-      const base = doc.createElement("base");
-      base.setAttribute("href", url);
-      doc.head.insertBefore(base, doc.head.firstChild);
-      return c.html(doc.documentElement?.outerHTML ?? "");
-   });
+   app.get("/", auth, admin, () => fetch(`${url}/index.html`));
+   app.get("/assets/*", (c) => fetch(`${url}${c.req.path}`));
 };
 
 export default apply;
