13a52e6f60feb28a19e63bb33cd231e5d776db34
sholvoir <sovar.he@gmail.com>
2026-08-13 16:44:28 -0400

    0.7.39

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
-import { emptyResponse, STATUS_CODE } from "@sholvoir/generic/http";
 import type { Hono } from "hono";
+import { proxy } from "hono/proxy";
 import admin from "../mid/admin.ts";
 import auth from "../mid/auth.ts";
 import renew from "../mid/renew.ts";
@@ -7,17 +7,8 @@ import renew from "../mid/renew.ts";
 const url = "https://www.micinfotech.com/dict";
 
 const apply = (app: Hono) => {
-   app.get("/", auth, admin, renew, async (c) => {
-      const resp = await fetch(`${url}/index.html`);
-      if (!resp.ok) return emptyResponse(STATUS_CODE.NotFound);
-      const text = await resp.text();
-      return c.html(text);
-   });
-   app.get("/assets/*", async (c) => {
-      const resp = await fetch(`${url}${c.req.path}`);
-      if (!resp.ok) return emptyResponse(STATUS_CODE.NotFound);
-      return resp;
-   });
+   app.get("/", auth, admin, renew, () => proxy(`${url}/index.html`));
+   app.get("/assets/*", (c) => proxy(`${url}${c.req.path}`));
 };
 
 export default apply;
