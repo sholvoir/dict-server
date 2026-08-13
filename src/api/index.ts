546774d008198c8ef5d7e2572702cd0d57348929
sholvoir <sovar.he@gmail.com>
2026-08-13 16:32:40 -0400

    0.7.37

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,8 @@
+import {
+   emptyResponse,
+   STATUS_CODE,
+   textResponse,
+} from "@sholvoir/generic/http";
 import type { Hono } from "hono";
 import admin from "../mid/admin.ts";
 import auth from "../mid/auth.ts";
@@ -6,8 +11,18 @@ import renew from "../mid/renew.ts";
 const url = "https://www.micinfotech.com/dict";
 
 const apply = (app: Hono) => {
-   app.get("/", auth, admin, renew, () => fetch(`${url}/index.html`));
-   app.get("/assets/*", (c) => fetch(`${url}${c.req.path}`));
+   app.get("/", auth, admin, renew, async () => {
+      const resp = await fetch(`${url}/index.html`);
+      if (!resp.ok) return emptyResponse(STATUS_CODE.NotFound);
+      const text = await resp.text();
+      return textResponse(text);
+   });
+   app.get("/assets/*", async (c) => {
+      const resp = await fetch(`${url}${c.req.path}`);
+      if (!resp.ok) return emptyResponse(STATUS_CODE.NotFound);
+      const text = await resp.text();
+      return textResponse(text);
+   });
 };
 
 export default apply;
